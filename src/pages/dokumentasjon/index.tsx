45791ce109525e141667eb8687217ebcfdef06e8
Zekima <christianledaal@gmail.com>
2024-02-10 13:33:09 +0100

    Resized dokumentasjon images

--- src/pages/dokumentasjon/index.tsx
+++ src/pages/dokumentasjon/index.tsx
@@ -9,18 +9,18 @@ export default function IndexPage() {
       <div className="flex flex-col gap-y-2">
         <div className="flex gap-2">
           <div className="p-5 bg-gray-300 w-1/5">
-            <Image src="/docs.png" height={250} width={90} alt="" />
+            <Image src="/docs.png" height={125} width={90} alt="" />
             <p className="mt-2">Prosjektskisse →</p>
           </div>
           <div className="p-5 bg-gray-300 w-1/5">
-            <Image src="/docs.png" height={200} width={90} alt="" />
+            <Image src="/docs.png" height={125} width={90} alt="" />
             <p className="mt-2">Prosjektbeskrivelse →</p>
           </div>
         </div>
 
         <div className="flex gap-2">
           <div className="p-5 bg-gray-300 w-1/5">
-            <Image src="/slides.png" height={250} width={90} alt="" />
+            <Image src="/slides.png" height={125} width={90} alt="" />
             <p className="mt-2">Milepæl 1 Presentasjon →</p>
           </div>
         </div>
